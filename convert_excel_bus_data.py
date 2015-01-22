e0c3804b7a10797a1571b6f600a02eddf7ed7f12
Takahiro Endo <endo@meme.hokudai.ac.jp>
2015-01-22 12:48:10 +0900

    運行曜日の対応
    Excelマクロで運行曜日の行を追加したデータに対応。
    オリジナルのレイアウトには対応していない。

--- convert_excel_bus_data.py
+++ convert_excel_bus_data.py
@@ -13,7 +13,7 @@
     2.系統ー運行時刻データ
         KEITOU_SCHEDULE : Array[ Schedule ]; Scheduleオブジェクトのリスト
         Schedule : { 
-                    SCHEDULE_DAYTYPE: Int; 運行曜日(0: 平日，1: 土日祝)
+                    SCHEDULE_DAYTYPE: Int; 運行曜日(0: 平日，1: 土日祝, 9:不明)
                     SCHEDULE_TIME: Array[ String ]; 時刻のリスト
                     }
     
@@ -24,10 +24,11 @@ import os
 import xlrd
 import json
 
-# Excelふぁいるの
+# Excelファイルのレイアウト
 ROW_TOP = 0
 ROW_HEADER = 1
-ROW_DATA_START = 2
+ROW_DAYTYPE = 2
+ROW_DATA_START = 3
 
 COL_BUSSTOPS = 0
 COL_KEITOU = 1
@@ -48,9 +49,6 @@ DAYTYPE_WORKDAY = 0 # 平日
 DAYTYPE_HOLIDAY = 1 # 土日祝
 DAYTYPE_UNKNOWN = 9 # 不明
 
-# TODO:運行曜日判定
-WORKDAY_XF_VALUES = [75,79,80,86,87]
-HOLIDAY_XF_VALUES = [78,81,82,83,88,89]
 
 def convertExcelFile(filepath):
     busdata = {}
@@ -124,20 +122,21 @@ def convertExcelSheet(book, sheet_name, route_name):
     keitou_schedule[KEITOU_NUMBER] = keitou_number
     
     operations = []
+    # 列：運行時刻
     for ncol in range(COL_DATA_START, sheet.ncols):
         cells = sheet.col(ncol) 
         if cells[ROW_HEADER].ctype == xlrd.XL_CELL_EMPTY:
             break
         elif cells[ROW_HEADER].ctype == xlrd.XL_CELL_NUMBER:
             op = {}
-            # TODO:運行曜日判定
-            if cells[ROW_HEADER].xf_index in WORKDAY_XF_VALUES:
-                op[SCHEDULE_DAYTYPE] = DAYTYPE_WORKDAY
-            elif cells[ROW_HEADER].xf_index in HOLIDAY_XF_VALUES:
-                op[SCHEDULE_DAYTYPE] = DAYTYPE_HOLIDAY
-            else:
-                op[SCHEDULE_DAYTYPE] = DAYTYPE_UNKNOWN
-            
+            # 運行曜日判定
+            if cells[ROW_DAYTYPE].ctype == xlrd.XL_CELL_TEXT:
+                if cells[ROW_DAYTYPE].value == u'平日':
+                    op[SCHEDULE_DAYTYPE] = DAYTYPE_WORKDAY
+                elif cells[ROW_DAYTYPE].value == u'土日祝':
+                    op[SCHEDULE_DAYTYPE] = DAYTYPE_HOLIDAY
+                else:
+                    op[SCHEDULE_DAYTYPE] = DAYTYPE_UNKNOWN
             
             op[SCHEDULE_TIME] = []
             for cell in cells[ROW_DATA_START:]:
